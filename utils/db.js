e48567c50aaab9f11d9019ad5fb85ce6283cbc33
Dan <daniel.liemng@gmail.com>
2022-01-27 18:28:28 -0500

    connect MongoDB

---
+++ utils/db.js
@@ -0,0 +1,41 @@
+import mongoose from 'mongoose';
+
+const connection = {};
+
+const connectDB = async () => {
+  if (connection.isConnected) {
+    console.log('MongoDB Already Connected!!!');
+    return;
+  }
+
+  if (mongoose.connections.length > 0) {
+    connection.isConnected = mongoose.connections[0].readyState;
+
+    if (connection.isConnected === 1) {
+      console.log('Use previous connection');
+      return;
+    }
+
+    await mongoose.disconnect();
+  }
+
+  // Connect
+  const db = await mongoose.connect(process.env.MONGO_URI);
+  console.log('New Connection !!!');
+  connection.isConnected = db.connections[0].readyState;
+};
+
+const disconnectDB = async () => {
+  if (connection.isConnected) {
+    if (process.env.NODE_ENV === 'production') {
+      await mongoose.disconnect();
+      connection.isConnected = false;
+    } else {
+      console.log('Not Connected');
+    }
+  }
+};
+
+const db = { connectDB, disconnectDB };
+
+export default db;
